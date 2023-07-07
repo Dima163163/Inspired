7d0628f7ea6654ac10d97b9f5d956c68776f268b
Dima163163 <khlopovdima@gmail.com>
2023-07-07 19:01:24 +0400

    delete console.log

--- src/Components/ColorList/ColorList.jsx
+++ src/Components/ColorList/ColorList.jsx
@@ -3,9 +3,7 @@ import s from './ColorList.module.scss';
 import { Color } from './Color/Color';
 
 export const ColorList = ({ colors }) => {
-	console.log(colors)
 	const { colorList } = useSelector(state => state.color);
-	console.log(colorList);
 
 	return (
 		<ul className={s.colorList}>
